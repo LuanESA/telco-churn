c8972915a3848221eebcd7ffcea8bdaf11a7f1d6
Luan <luaneduard7@gmail.com>
2026-04-17 20:07:55 -0300

    dados e config

---
+++ config.py
@@ -0,0 +1,16 @@
+# Dados brutos
+RAW_DATA_PATH = "raw/WA_Fn-UseC_-Telco-Customer-Churn.csv"
+ 
+# Dados exportados
+PROCESSED_DATA_PATH = "processed/dados_finais.csv"
+ 
+# Colunas de serviços adicionais contratados
+COLUNAS_SERVICOS = [
+    'OnlineSecurity',
+    'OnlineBackup',
+    'DeviceProtection',
+    'TechSupport',
+    'StreamingTV',
+    'StreamingMovies',
+]
+ 
--- v1_dados.py
+++ v1_dados.py
@@ -1,5 +1,7 @@
 import pandas as pd
-
+from config import RAW_DATA_PATH
+ 
+ 
 def dados_arquivos():
-    df = pd.read_csv("raw/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+    df = pd.read_csv(RAW_DATA_PATH)
     return df
